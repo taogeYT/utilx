b580f1a6f3e30f6f64bfee2041fb951b31a93ede
liyatao <li_yatao@outlook.com>
2020-04-26 23:19:24 +0800

    deploy 代码重构

--- utilx/deploy.py
+++ utilx/deploy.py
@@ -1,16 +1,8 @@
+# -*- coding: utf-8 -*-
 """
-supervisor
-"""
-import fire
-import sys
-import os
-import getpass
+@time: 2020/4/26 2:11 下午
+@desc: 基于supervisor的项目一键自启动挂后台工具
 
-from plan import Plan, PlanError
-
-__all__ = ["Supervisor", "set_env", "Environment"]
-
-"""
 配置说明
 注意：如果是执行python程序，命令python启动命令要加-u参数，example: python -u app.py
 
@@ -20,210 +12,188 @@ b) 启动即退出，exit code为1, 那么supervisor会在重试3试后停止重
 c) 启动3秒后退出，exit code为1, 那么supervisor会无限重启程序，无视startretries。
 d) 启动3秒后退出，exit code为0, 那么supervisor不会重启，进程状态为EXIT。
 """
-config_template = """
-[program:{project}_{name}]
+import sys
+import os
+import getpass
+from abc import ABC, abstractmethod
 
-command     = {command}
-directory   = {home}
-user        = {user}
-autorestart = {autorestart}
-startsecs   = {startsecs}
-{env_config}
-redirect_stderr         = {redirect_stderr}
-stdout_logfile_maxbytes = {stdout_logfile_maxbytes}
-stdout_logfile_backups  = {stdout_logfile_backups}
-stdout_logfile          = {logfile}
-killasgroup=true
-stopasgroup=true
-"""
+import fire
+from plan import Plan, PlanError
 
 
-class Supervisor(object):
-    """
-    example:
-        commands = [("celery_task", "celery worker -l INFO -A webapp.api.celery")]
-        class T(Environment):
-            def task1(self):
-                pass
-        Supervisor(T, commands).export()
-    """
-    config = {"autorestart": True, "startsecs": 5, "redirect_stderr": "true",
-              "stdout_logfile_maxbytes": "200MB", "stdout_logfile_backups": 5}
+class Config(object):
+    user = getpass.getuser()
+    autorestart = True
+    startsecs = 5
+    redirect_stderr = "true"
+    stdout_logfile_maxbytes = "200MB"
+    stdout_logfile_backups = 5
+    killasgroup = "true"
+    stopasgroup = "true"
+    template = """[program:{project}_{name}]\n\n{config_content}\n"""
 
-    def __init__(self, script=None, commands=None, config=None, env_name="PYENV"):
+    def __init__(self, home, config):
         if config is None:
             config = {}
-        if commands is None:
-            commands = []
-        self.config.update(config)
-        self.script = script
-        self._commands = commands
-        file_path = self.__module__
-        self.python = os.path.join(sys.prefix, "bin", "python")
-        self.home = os.path.dirname(os.path.abspath(file_path))
+        self.tasks = []
+        self.home = home
         self.project = os.path.basename(self.home)
-        self._env_name = env_name
-        self.env = os.getenv(env_name)
-        python = os.path.join(sys.prefix, "bin")
-        path = f'{python}:%(ENV_PATH)s'
-        if self.env:
-            self.env_config = f'environment=PATH={path},{env_name}={self.env}'
+        self.config = {
+            "user": self.user,
+            "directory": self.home,
+            "startsecs": self.startsecs,
+            "autorestart": self.autorestart,
+            "killasgroup": self.killasgroup,
+            "stopasgroup": self.killasgroup,
+            "redirect_stderr": self.redirect_stderr,
+            "stdout_logfile_backups": self.stdout_logfile_backups,
+            "stdout_logfile_maxbytes": self.stdout_logfile_maxbytes,
+            "environment": f'PATH={os.path.join(sys.prefix, "bin")}:%(ENV_PATH)s'
+        }
+        self.config.update(config)
+
+    @staticmethod
+    def convert_config_text(config):
+        if config is None:
+            return ""
         else:
-            self.env_config = f"environment=PATH={path}"
-        self.cron = Plan(self.project)
-        self.config.update(
-            {"home": self.home, "user": getpass.getuser(), "project": self.project, "env_config": self.env_config})
+            return "\n".join([f"{k: <25}= {v}" for k, v in config.items()])
 
-    def _get_script_names(self):
-        except_names = [name for name in self.script.env.keys() if self.env not in self.script.env[name]]
-        names = [name for name in dir(self.script) if name != "env" and name not in except_names and not name.startswith("_")]
-        return names
+    def add(self, name, command):
+        logfile = self.get_log_path(name)
+        config = {"command": command, "stdout_logfile": logfile, **self.config}
+        config_content = self.convert_config_text(config)
+        task_config = self.template.format(project=self.project, name=name, config_content=config_content)
+        self.tasks.append(task_config)
 
-    def _gen_scripts_conf(self):
-        configs = []
-        for name in self._get_script_names():
-            logfile = os.path.join(self.home, "logs", f"{name}.log")
-            script = os.path.abspath(sys.modules[self.script.__module__].__file__)
-            # script = "{self.home}/run.py"
-            command = f"{self.python} -u {script} {name}"
-            configs.append(config_template.format(command=command, name=name, logfile=logfile, **self.config))
-        return configs
+    def get_log_path(self, name):
+        return os.path.join(self.home, "logs", f"{name}.log")
 
-    def _gen_cmd_conf(self):
-        configs = []
-        for name, command, *envs in self._commands:
-            if not envs or self.env in envs:
-                logfile = os.path.join(self.home, "logs", f"{name}.log")
-                configs.append(config_template.format(command=command, name=name, logfile=logfile, **self.config))
-        return configs
-
-    def gen_config(self):
-        script_config = self._gen_scripts_conf() if self.script else []
-        cmd_config = self._gen_cmd_conf()
-        content = "".join([*script_config, *cmd_config])
+    def export(self, content=None):
+        if content is None:
+            content = "\n".join(self.tasks)
         print(content)
-        return content
-
-    def update_conf_file(self, content, file_path):
-        file = self.get_file(file_path)
+        file = f"{self.project}.utilx.ini"
         with open(file, "w") as f:
             f.write(content)
+        log_path = os.path.join(self.home, "logs")
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+        return file
+
+
+class BaseDeployment(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def help(self):
+        print(f"Usage: python {sys.argv[0]} [start|stop|restart]")
+
+    def execute(self, option):
+        """
+        [start|stop|restart]
+        """
+        actions = {
+            "start": self.start,
+            "stop": self.stop,
+            "restart": self.restart,
+        }
+        actions.get(option, self.help)()
+
+    def deploy(self):
+        fire.Fire(self.execute)
+
+
+class Deployment(BaseDeployment):
+    python = os.path.join(sys.prefix, "bin", "python")
+
+    def __init__(self, commands=None, script=None, crons=None, config=None, need_sudo=False):
+        self.need_sudo = need_sudo
+        self.crons = self.get_crons(crons)
+        self.scripts = self.get_scripts(script)
+        self.commands = self.get_commands(commands)
+        self.home = os.path.dirname(os.path.abspath(self.__module__))
+        # self.pyenv = os.getenv(env_name)
+        self.config = Config(self.home, config)
+        self.cron = Plan(self.config.project)
+        self.init_tasks()
+
+    def init_tasks(self):
+        for name, cmd in self.commands:
+            self.config.add(name, cmd)
+        for name, cmd in self.scripts:
+            self.config.add(name, cmd)
+        for cron in self.crons:
+            self.cron.command(cron)
+
+    # def get_scripts(self, script_obj):
+    #     if script_obj is None:
+    #         return []
+    #     if isinstance(script_obj, Environment):
+    #         # 存放脚本文件名称
+    #         script_file_name = os.path.abspath(sys.modules[script_obj.__module__].__file__)
+    #         except_names = [name for name in script_obj.envs if self.pyenv not in script_obj.envs[name]]
+    #         scripts = [name for name in dir(script_obj) if
+    #                    name != "envs" and name not in except_names and not name.startswith("_")]
+    #         return [(script, f"{self.python} -u {script_file_name} {script}") for script in scripts]
+    #     else:
+    #         raise TypeError("无效的script_obj类型")
+    def get_scripts(self, script_obj):
+        if script_obj is None:
+            return []
+        else:
+            # 存放脚本文件名称
+            script_file_name = os.path.abspath(sys.modules[script_obj.__module__].__file__)
+            scripts = [name for name in dir(script_obj) if not name.startswith("_")]
+            return [(script, f"{self.python} -u {script_file_name} {script}") for script in scripts]
+
+    def get_commands(self, commands):
+        name_commands = []
+        if commands:
+            for name, command, *envs in commands:
+                # if not envs or self.pyenv in envs:
+                name_commands.append((name, command))
+        return name_commands
+
+    def get_crons(self, crons_config):
+        crons = []
+        if crons:
+            for command, *envs in crons_config:
+                # if not envs or self.pyenv in envs:
+                crons.append(command)
+        return crons
 
     def update_supervisor(self, file):
-        if file is None:
-            _file = self.get_file(file)
-            log_dir = os.path.join(self.home, "logs")
-            os.makedirs(log_dir) if not os.path.exists(log_dir) else None
-            code1 = os.system(f"sudo mv {_file} /etc/supervisord.d/")
+        if self.need_sudo:
+            code1 = os.system(f"sudo mv {file} /etc/supervisord.d/")
             code2 = os.system("sudo supervisorctl update")
-            return code1 == 0 and code2 == 0
-
-    def add_crontab(self, *args, **kwargs):
-        """
-        example:
-            setup = Setup()
-            setup.add_crontab('ls /tmp', every='1.day', at='12:00')
-
-        every can be:
-
-        [1-60].minute [1-24].hour [1-31].day
-        [1-12].month [1].year
-        jan feb mar apr may jun jul aug sep oct nov dec
-        sun mon tue wed thu fri sat weekday weekend
-        or any fullname of month names and day of week names
-        (case insensitive)
-
-        at can be:
-
-        when every is minute, can not be set
-        when every is hour, can be minute.[0-59]
-        when every is day of month, can be minute.[0-59], hour.[0-23]
-        when every is month, can be day.[1-31], day of week,
-                             minute.[0-59], hour.[0-23]
-        when every is day of week, can be minute.[0-59], hour.[0-23]
-
-        at can also be multiple at values seperated by one space.
-        """
-        self.cron.command(*args, **kwargs)
-
-    def export(self):
-        """
-        supervisor配置导出
-        file 默认路径 /etc/supervisord.d/
-        """
-        fire.Fire(self._export)
-
-    def get_file(self, file):
-        if file is None:
-            _file = f"{self.project}.ini"
         else:
-            _file = file
-        return _file
+            code1 = os.system(f"mv {file} /etc/supervisord.d/")
+            code2 = os.system("supervisorctl update")
+        return code1 or code2
 
-    def _export(self, cmd):
-        """
-        参数说明
-        --cmd: [start|stop|restart]
-        --file: 导出文件
-        """
-        if cmd == "start":
-            self._start(None)
-        elif cmd == "stop":
-            self._stop(None)
-        elif cmd == "restart":
-            self._stop(None)
-            self._start(None)
-        else:
-            print(f"Usage: python {sys.argv[0]} [start|stop|restart]")
-
-    def _start(self, file):
-        config_content = self.gen_config()
-        self.update_conf_file(config_content, file)
-        result = self.update_supervisor(file)
-        if config_content:
-            if self.env is None:
-                print(f"WARN: 环境变量'{self._env_name}'未配置")
-            else:
-                if result:
-                    self.cron.run("update")
-                    print(f"success, 当前环境: {self._env_name}={self.env}")
-                else:
-                    print(f"failed, supervisor命令执行异常, 当前环境: {self._env_name}={self.env}")
-        else:
-            print("未找到可用配置")
-
-    def _stop(self, file):
-        config_content = ""
-        self.update_conf_file(config_content, file)
-        self.update_supervisor(file)
+    def update_cron(self, action):
         try:
-            self.cron.run("clear")
+            self.cron.update_crontab(action)
         except PlanError:
-            print("没有要清空的crontab任务")
-        print("已停止所有执行任务")
+            pass
 
+    def start(self):
+        config_file = self.config.export()
+        self.update_cron("update")
+        return self.update_supervisor(config_file)
 
-class _EnvironmentMeta(type):
-    pass
-    # def __init__(cls, name, bases, dct):
-    #     cls._env = {}
-    #     super().__init__(name, bases, dct)
-    #     inspect.getmembers(cls, inspect.ismethod)
-
-
-class Environment(object, metaclass=_EnvironmentMeta):
-    env = {}
-
-
-def set_env(envs):
-    if not isinstance(envs, (list, tuple)):
-        print("ERROR: set_env 参数必须是列表类型")
-        sys.exit(1)
-
-    def decorator(func):
-        Environment.env[func.__name__] = envs
-
-        def wrapper(*args, **kw):
-            return func(*args, **kw)
-        return wrapper
-    return decorator
+    def stop(self):
+        config_file = self.config.export(content="")
+        self.update_cron("clear")
+        return self.update_supervisor(config_file)
